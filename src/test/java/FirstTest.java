a0e41aa340d52b6603dd3b400d1cef140084b134
saahoramesh <rameshnitw08452@gmail.com>
2024-08-31 12:00:00 +0530

    First Test Commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,5 @@
 import org.example.Main;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -11,7 +12,10 @@ public class FirstTest extends Main {
     @Test
     public  void loginTest()
     {
-       driver.get("https://www.chess.com/");
-       driver.manage().window().maximize();
+       driver.get("https://rahulshettyacademy.com/client");
+        driver.manage().window().maximize();
+       driver.findElement(By.id("userEmail")).sendKeys("rameshnitw08452@gmail.com");
+       driver.findElement(By.id("userPassword")).sendKeys("N0t@llowed");
+       driver.findElement(By.id("login")).click();
     }
 }
